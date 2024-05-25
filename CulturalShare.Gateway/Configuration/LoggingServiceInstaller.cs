7f92322971adbc8a665859d3bc19070441f6030b
Oleksiy <oleksiy.kondratyuk.work@gmail.com>
2024-05-25 15:09:37 +0300

    Cul-13 Graylog configuration was added

--- CulturalShare.Gateway/Configuration/LoggingServiceInstaller.cs
+++ CulturalShare.Gateway/Configuration/LoggingServiceInstaller.cs
@@ -2,6 +2,10 @@
 using CulturalShare.Gateway.Configuration.Base;
 using Serilog;
 using Serilog.Core;
+using Serilog.Sinks.Graylog;
+using CulturalShare.Common.Helper.EnvHelpers;
+using CulturalShare.Common.Helper.Configurations;
+using Newtonsoft.Json;
 
 namespace CulturalShare.Gateway.Configuration;
 
@@ -9,6 +13,9 @@ public class LoggingServiceInstaller : IServiceInstaller
 {
     public void Install(WebApplicationBuilder builder, Logger logger)
     {
+        var sortOutCredentialsHelper = new SortOutCredentialsHelper(builder.Configuration);
+        var graylogConfig = sortOutCredentialsHelper.GetGraylogConfiguration();
+
         builder.Host.UseSerilog((context, config) =>
         {
             var configuration = builder.Configuration;
@@ -16,6 +23,13 @@ public class LoggingServiceInstaller : IServiceInstaller
             config.Enrich.WithCorrelationIdHeader(LoggingConsts.CorrelationIdHeaderName);
             config.Enrich.WithProperty(LoggingConsts.Environment, Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
             config.ReadFrom.Configuration(configuration);
+
+            config.WriteTo.Graylog(new GraylogSinkOptions()
+            {
+                HostnameOrAddress = graylogConfig.Host,
+                Port = graylogConfig.Port,
+                TransportType = graylogConfig.TransportType,
+            });
         });
 
         logger.Information($"{nameof(LoggingServiceInstaller)} installed.");
